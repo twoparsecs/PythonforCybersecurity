af460249ba6c3e0fd5d8919ec9b9e8ffe3f8413f
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-01 20:08:09 -0700

    Added code

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
-# Created 
+# Created by Justin Gardner 7/1/2024
+
+yourName = input("What is your name? ")
+print("Hello {0}".format(yourName))
+print("The only easy day was yesterday.")
+
+age = int(input("What is your age? "))
+futureAge = str(age + 2)
+message = " in two years you will be "
+print(yourName + message + futureAge)
