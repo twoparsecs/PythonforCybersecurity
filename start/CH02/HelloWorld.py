39e8f67bb4bd8728c77346ad72f8add91fcd3df2
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-05 18:59:40 -0700

    Wrote a simple script

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
-# A simple "Hello World" script in python
-# Created 
+# The script generates a promt asking if it is a good day. -40 points
+# The script takes the text entry from the user and stores it as a variable. -30 points
+# The script provides the appropriate responses. Test case one passes, test case two passes,
+# test case 3 passes. -30 points
+# Created by Justin Gardner on 7-5-2024
+
+day = input("Is today a good day? Please answer y/n... ")
+if day == "y":
+    print("Yes, today is a great day.")
+elif day == "n":
+    print("I am sorry to hear that... :(")
+else:
+    print("You did not make the right selection. Goodbye...")
