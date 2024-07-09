cb67f2b87fe89cedf9c951af4c3ac85f19c9322f
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-08 22:29:19 -0700

    ping1

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
-# By 
+# By Justin Gardner
+
+
+import platform
+import os
+
+ip = "127.0.0.1"
+
+ping_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+exit_code = os.system(ping_cmd)
+
+print(exit_code)
