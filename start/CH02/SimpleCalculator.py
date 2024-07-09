88d874c998f5a96d8c0f622672da60750323c8c8
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-08 22:11:53 -0700

    Simple Calc

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
-# Created 
+# Created by Justin Gardner
+
+first_num = float(input("What is the first number? "))
+activity = input("What activity? ( + - * /) ")
+second_num = float(input("What is the second number? "))
+
+if activity == "+":
+    print(first_num + second_num)
+if activity == "-":
+    print(first_num - second_num)
+if activity == "*":
+    print(first_num * second_num)
+if activity == "/":
+    print(first_num / second_num)
