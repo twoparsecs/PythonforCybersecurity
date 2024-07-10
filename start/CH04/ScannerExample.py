894e1ee0c6fcbf613e342604cbb62cead2ac4b6f
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-10 10:05:12 -0700

    nn

--- start/CH04/ScannerExample.py
+++ start/CH04/ScannerExample.py
@@ -2,4 +2,19 @@
 # Port scanner example 
 # Use 'pip3 install python-nmap' to install modules
 # Use 'sudo apt -y install nmap' to install nmapccc
-# By 
+# By Justin Gardner
+
+import nmap
+
+target_address = input("What IP Adress would you like to scan? ")
+
+port_start = 1
+port_end = 100
+scanner = nmap.PortScanner()
+
+print("Scanning {0}".format(target_address))
+
+for port in range(port_start, port_end + 1):
+    result = scanner.scan(target_address, str(port))
+    port_status = result['scan'][target_address]['tcp'][port]['state']
+    print("\tPort: {0} is {1}".format(port, port_status))
