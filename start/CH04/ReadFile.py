ce5ec9ce2558d3a548e03143661f2e121db3628f
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-09 21:59:44 -0700

    help

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Justin Gardner
+
+ip_file = open("ips.txt", "r")
+ip_addresses = ip_file.read()
+print(ip_addresses)
+
+ip_file.close()
--- start/CH04/pinger5.py
+++ start/CH04/pinger5.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 # Fifth example of pinging from Python
 # Reading IPs from a file
-# By 
+# By Justin Gardner
+
+import platform
+import os
+
+def ping_host(ip):
+    current_os = platform.system().lower()
+    if current_os == "windows":
+        ping_cmd = f"ping -n 1 -w 2 {ip} > nul"
+    else:
+        ping_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+    exit_code = os.system(ping_cmd)
+    return exit_code
+
+def import_adresses():
+    lines = []
+    f = open("ips.txt", "r")
+    for line in f:
+        line = line.strip()
+        lines.append(line)
+    return lines
+    
+ip_address = import_adresses()
+
+for ip in ip_addresses:
+    exit_code = ping_host(ip)
+    if exit_code == 0:
+        print("{0} is online.".format(ip))
+
