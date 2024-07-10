ce489b4d812fad32b370ffe9f707a170e03f4882
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-09 21:29:38 -0700

    pinggggg

--- start/CH03/pinger4.py
+++ start/CH03/pinger4.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Fourth example of pinging from Python
-# By 
+# By Justin Gardner
+
+import platform
+import os
+
+def ping_host(ip):
+    current_os = platform.system().lower()
+    if current_os == "windows":
+        ping_cmd = f"ping -n 1 -w 2 {ip} > nul"
+    else:
+        ping_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+    exit_code = os.system(ping_cmd)
+    return exit_code
+
+ip_prefix = "192.168.0."
+
+for final_octet in range(254):
+    ip = ip_prefix + str(final_octet +1)
+    exit_code = ping_host(ip)
+    if exit_code == 0:
+        print("{0} is online".format)
