7f845b11b7b3361e7004edaad726106e2995eaba
twoparsecs <gardner.justin@student.greenriver.edu>
2024-07-27 16:36:03 -0700

    ROT13

--- start/CH05/ASCIIgen.py
+++ start/CH05/ASCIIgen.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 # ASCII generator
 # Uses chr() to create ASCII characters
-# By 
+# By Justin Gardner
+
+for i in range(127):
+    print ("{0}\t'{1}'".format(i, chr(i)))
--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
+
 # Script that encrypts/decrypts text using ROT13
-# By 
+
+# --- ROT13 ---
+# By Justin Gardner 
+# July 27, 2024
+
+# Prompt the user for the source messaage to encrypt
+
+source_message = input("What message would you like to encrypt? ")
+source_message = source_message.upper()
+cipher_message = ""
+
+# Loop through each letter in the source message
+for letter in source_message:
+    # Convert to ascii equivalent
+    ascii_num = ord(letter)
+    # Check to see if an alphabetic character(a-z) character,
+    # if not, skip
+    if ascii_num >= 65 and ascii_num <= 90:
+        # Add 13 to ascii_num to "shift" it by 13
+        new_ascii = ascii_num + 13
+        if new_ascii > 90:
+            new_ascii = new_ascii - 26
+        cipher_message = cipher_message +chr(new_ascii)
+    else:
+        cipher_message = cipher_message + chr(ascii_num)
+
+print("Message has been converted:")
+print(cipher_message)
